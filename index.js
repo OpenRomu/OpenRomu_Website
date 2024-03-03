f9561de9314f009ee5c9a278e9de63b4fa64abc9
Stalker2106 <contact@maximemartens.fr>
2024-03-03 02:09:14 +0100

    set main to be called index.html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function main() {
     nunjucks.configure({ autoescape: true })
     const outputPath = './dist'
     const templatesPath = './templates'
-    const pages = ['home', 'scores', 'archives']
+    const pages = ['index', 'scores', 'archives']
     if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath)
     for (const page of pages) {
         const navigation = nunjucks.render(`${templatesPath}/navigation.html`, { page })
@@ -14,7 +14,7 @@ async function main() {
             navigation,
             content
         }
-        const pageHtml = nunjucks.render(`${templatesPath}/index.html`, pageData)
+        const pageHtml = nunjucks.render(`${templatesPath}/main.html`, pageData)
         fs.writeFileSync(`${outputPath}/${page}.html`, pageHtml)
     }
 }
