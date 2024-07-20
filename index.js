25f4d82b60a9355c52ceea37e117b2abcca7bdd8
Stalker2106 <contact@maximemartens.fr>
2024-07-20 23:55:49 +0200

    polished sections, removed hardcoded data

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const nunjucks = require('nunjucks')
 const fs = require('fs')
 
+const outputPath = './dist'
+const templatesPath = './templates'
+const contentPath = './content'
+
 async function main() {
     nunjucks.configure({ autoescape: true })
-    const outputPath = './dist'
-    const templatesPath = './templates'
-    const pages = ['index', 'scores', 'archives']
+    const pages = ['index', 'howtoplay', 'archives'/* 'scores'*/]
     if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath)
+    // Iterate on all pages, render them and export as HTML in outputPath
     for (const page of pages) {
         const navigation = nunjucks.render(`${templatesPath}/navigation.html`, { page })
-        const content = await fs.readFileSync(`${templatesPath}/pages/${page}.html`, 'utf8')
+        let pageContent = null
+        // Render page content (archive for examples load data from JSON)
+        if (page === 'archives') {
+            var archivesData = {
+                versions: JSON.parse(fs.readFileSync(`${contentPath}/versions.json`, 'utf8')),
+                maps: JSON.parse(fs.readFileSync(`${contentPath}/maps.json`, 'utf8'))
+            }
+            pageContent = nunjucks.render(`${templatesPath}/pages/${page}.html`, archivesData)
+        } else {
+            pageContent = await fs.readFileSync(`${templatesPath}/pages/${page}.html`, 'utf8')
+        }
         const pageData = {
             navigation,
-            content
+            pageContent
         }
         const pageHtml = nunjucks.render(`${templatesPath}/main.html`, pageData)
         fs.writeFileSync(`${outputPath}/${page}.html`, pageHtml)
